8de2118bb7bed01981b368a544cd0432c81f3cb1
AmrTarek1097 <106562748+AmrTarek1097@users.noreply.github.com>
2023-06-18 21:54:06 +0300

    Add files via upload

--- CRUDS system/js/script.js
+++ CRUDS system/js/script.js
@@ -3,15 +3,13 @@ let price = document.getElementById("price");
 let device = document.getElementById("device");
 let disc = document.getElementById("disc");
 let search = document.getElementById("search");
-let productContainer= [];
-let currentIndex ;
-
+let productContainer = [];
+let currentIndex;
 
 if (localStorage.getItem("data") != null) {
   productContainer = JSON.parse(localStorage.getItem("data"));
   displayProduct(productContainer);
-};
-
+}
 
 function addproduct() {
   let product = {
@@ -38,7 +36,6 @@ function reset() {
 }
 
 function displayProduct(productContainer) {
-
   let container = ``;
 
   for (let index = 0; index < productContainer.length; index++) {
@@ -56,14 +53,19 @@ function displayProduct(productContainer) {
 }
 
 function deleteProduct(indexNumber) {
-
-productContainer.splice(indexNumber,1);
-localStorage.setItem("data", JSON.stringify(productContainer));
-displayProduct(productContainer);
+  productContainer.splice(indexNumber, 1);
+  localStorage.setItem("data", JSON.stringify(productContainer));
+  displayProduct(productContainer);
 }
 
-function updateProduct(indexNumber){
+function deleteAllProducts(indexNumber) {
+  productContainer.splice(indexNumber);
+  localStorage.setItem("data", JSON.stringify(productContainer));
 
+  displayProduct(productContainer);
+}
+
+function updateProduct(indexNumber) {
   currentIndex = indexNumber;
   console.log(productContainer);
   pname.value = productContainer[indexNumber].productname;
@@ -71,37 +73,43 @@ function updateProduct(indexNumber){
   device.value = productContainer[indexNumber].productdevice;
   disc.value = productContainer[indexNumber].productdisc;
 
+  document.getElementById("my-alert-update").classList.remove("d-none");
+
   document.getElementById("updatebtn").classList.remove("d-none");
   document.getElementById("addbtn").classList.add("d-none");
+
+  setTimeout(() => {
+    document.getElementById("my-alert-update").classList.add("d-none");
+  }, 3000);
   // console.log(indexNumber);
-};
+}
 
-function updateConfirmation(){
-
-
+function updateConfirmation() {
   let product = {
     productname: pname.value,
     productprice: price.value,
     productdevice: device.value,
     productdisc: disc.value,
   };
-  productContainer.splice(currentIndex,1 , product);
+  productContainer.splice(currentIndex, 1, product);
   localStorage.setItem("data", JSON.stringify(productContainer));
   reset();
   displayProduct(productContainer);
 
   document.getElementById("updatebtn").classList.add("d-none");
   document.getElementById("addbtn").classList.remove("d-none");
-};
+}
 
 function searchProduct(term) {
-
   let container = ``;
 
   for (let index = 0; index < productContainer.length; index++) {
-if (productContainer[index].productname.toLowerCase().includes(term.toLowerCase())  ) {
-
-  container += `<tr>
+    if (
+      productContainer[index].productname
+        .toLowerCase()
+        .includes(term.toLowerCase())
+    ) {
+      container += `<tr>
     <td>${index}</td>
     <td>${productContainer[index].productname}</td>
     <td>${productContainer[index].productprice}</td>
@@ -110,12 +118,22 @@ if (productContainer[index].productname.toLowerCase().includes(term.toLowerCase(
     <td><button onclick='updateProduct(${index})' class="btn btn-outline-warning">Update</button></td>
     <td><button onclick='deleteProduct(${index})' class="btn btn-outline-danger">Delete</button></td>
     </tr>`;
-}
+    }
   }
-  document.getElementById("mytable").innerHTML = container;    
-  
+  document.getElementById("mytable").innerHTML = container;
 }
 
-
-
-
+function checkingEmpty() {
+  if (pname.value == "" || price.value == "" || device.value == "") {
+    document.getElementById("my-alert-empty").classList.remove("d-none");
+    setTimeout(() => {
+      document.getElementById("my-alert-empty").classList.add("d-none");
+    }, 2000);
+  } else {
+    addproduct();
+    document.getElementById("my-alert-success").classList.remove("d-none");
+    setTimeout(() => {
+      document.getElementById("my-alert-success").classList.add("d-none");
+    }, 2000);
+  }
+}
